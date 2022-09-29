4cf2004eb8c17cb3d9081eee252881ecd0302df7
Isabel <marisita2612@hotmail.com>
2022-09-29 14:27:16 -0500

    se sube repository de compra

--- src/repositories/comprarepository.js
+++ src/repositories/comprarepository.js
@@ -10,23 +10,17 @@ comprarepository.getById = (id) => {
     const context = pool()
     return context.query('select * from tiquete where id=$1', [id]) 
 }
-
-
-
-// repository.get = (username, contraseña) => {
-//   const context =  pool()
-//   return context.query('select * from login where username=$1 and contraseña=$2',[username,contraseña])
-// }
-// //de registro para crear uno new
-// repository.create = (registro) => {
-//   const context =  pool()
-//   return context.query('insert into registro (username,correo,contraseña) values ($1,$2,$3) RETURNING id',[registro.username,registro.correo, registro.contraseña])
-// }
-// repository.create = (tiquete) => {
-//   const context =  pool()
-//   return context.query('insert into tiquete (id,fechaEntrada,fechaSalida,numeroNinos,numeroAdultos,origen,vuelta) values ($1,$2,$3,$4,$5,$6,$7) RETURNING id',[tiquete.id,tiquete.fechaEntrada, tiquete.fechaSalida, tiquete.numeroNinos, tiquete.numeroAdultos, tiquete.origen, tiquete.vuelta])
-// }
-
-
-
-// module.exports = repository;
+comprarepository.create = (tiquete) =>{
+const context =  pool()
+  return context.query('insert into tiquete (id,fechaEntrada,fechaSalida,numeroNinos,numeroAdultos,origen,vuelta) values ($1,$2,$3,$4,$5,$6,$7)  RETURNING id',[tiquete.id,tiquete.fechaEntrada, tiquete.fechaSalida, tiquete.numeroNinos, tiquete.numeroAdultos, tiquete.origen, tiquete.vuelta] )
+}
+comprarepository.update = (tiquete) => {
+    const context =  pool()
+    return context.query('update tiquete SET fechaEntrada = $2, fechaSalida=$3 where id=$1',[tiquete.Id,tiquete.fechaEntrada,tiquete.fechaSalida])
+  }
+  
+  comprarepository.delete = (id) => {
+    const context =  pool()
+    return context.query('delete from tiquete where id=$1',[id])
+  }
+  module.exports = comprarepository;
